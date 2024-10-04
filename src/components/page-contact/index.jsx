7ae2715cafae62e66f57198668ba860197eddbec
yongqi8899 <1243994415@qq.com>
2024-10-04 13:44:42 +0200

    fix style

--- src/components/page-contact/index.jsx
+++ src/components/page-contact/index.jsx
@@ -3,7 +3,7 @@ import ContactForm from "./cpns/form.jsx";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col w-screen max-w-lg px-10 m-auto md:mt-40 md:flex-row">
+    <div className="flex flex-col max-w-screen-lg px-10 m-auto md:mt-40 md:flex-row">
       <ContactForm />
       <div className="w-full">
         <EarthCanvas />
