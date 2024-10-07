836a1bbbe100745310e4f889329c62d9ee4cdd67
yongqi8899 <1243994415@qq.com>
2024-10-07 12:53:39 +0200

    add navicon

--- src/components/page-home/cpns/me.jsx
+++ src/components/page-home/cpns/me.jsx
@@ -13,40 +13,42 @@ const ComMe = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-     <Canvas
-      camera={{
-        fov: 30,
-        position: [5, 10, 2],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <ContactShadows
-          opacity={0.42}
-          scale={10}
-          blur={1}
-          far={10}
-          resolution={256}
-          color="#000000"
-        />
-        <ambientLight intensity={2} />
-        <pointLight intensity={1} />
-        <primitive object={computer.scene} scale={0.2} position-y={0.25} />
-       
-        <group position={[1, -0.52, 0.7]}>
-          <Me rotation={[0, 0, -Math.PI / 2]} scale={1.2} />
-          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-            <boxGeometry />
-            <meshStandardMaterial color="#915EFF" wireframe />
-            <Edges color="#915EFF" lineWidth={2} />
-          </mesh>
-        </group>
-      </Suspense>
-    </Canvas>
+    <div className="absolute w-full h-full">
+      <Canvas
+        camera={{
+          fov: 30,
+          position: [5, 10, 2],
+        }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <ContactShadows
+            opacity={0.42}
+            scale={10}
+            blur={1}
+            far={10}
+            resolution={256}
+            color="#000000"
+          />
+          <ambientLight intensity={2} />
+          <pointLight intensity={1} />
+          <primitive object={computer.scene} scale={0.2} position-y={0.25} />
+
+          <group position={[1, -0.52, 0.7]}>
+            <Me rotation={[0, 0, -Math.PI / 2]} scale={1.2} />
+            <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+              <boxGeometry />
+              <meshStandardMaterial color="#915EFF" wireframe />
+              <Edges color="#915EFF" lineWidth={2} />
+            </mesh>
+          </group>
+        </Suspense>
+      </Canvas>
+    </div>
   );
 };
 export default ComMe;
