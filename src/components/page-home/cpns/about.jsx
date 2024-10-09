dd46b0f25992e4e4544744deaf287063c5a3855d
yongqi8899 <1243994415@qq.com>
2024-10-09 11:23:07 +0200

    fix cv

--- src/components/page-home/cpns/about.jsx
+++ src/components/page-home/cpns/about.jsx
@@ -37,7 +37,7 @@ const About = () => {
       }
       <div className="mt-10 ">
         <Link
-          to="https://drive.google.com/file/d/1ti_puly97jIu90EOzoNTAStuQwFT_f5j/view?usp=drive_link"
+          to="https://drive.google.com/file/d/1e-gofJnmOP8PNmqduIcgPeIxLuM5n9qF/view?usp=drive_link"
           target="_blank"
           className="px-6 py-2 text-lg font-bold border border-violet-500 violet-gradient rounded-xl hover:bg-violet-500 hover:text-white"
         >
